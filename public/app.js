b54049c5109b8bf411e0ecbca61d25f92a166ef8
Austin Walela <walela@mesozi.com>
2020-11-17 11:38:44 +0300

    completed main dashboard

---
+++ public/app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+  data() {
+    return {
+      forms: [
+        { title: "Form 001", questions: ["Question 01", "Question 02", "Question 03"] },
+        { title: "Form 002", questions: ["Question 01", "Question 02", "Question 03"] },
+      ],
+    };
+  },
+}).mount("#root");
