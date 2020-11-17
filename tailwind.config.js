9eea74314b3c2dcf65537d134c93b3cdc8277975
Austin Walela <walela@mesozi.com>
2020-11-17 08:17:40 +0300

    added search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,16 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          default: "#03a9f4",
+          lighter: "#1dc3ff",
+          darker: "#0090db",
+        },
+      },
       fontFamily: {
         primary: ["Manrope", "sans-serif"],
+        secondary: ["Inter", "sans-serif"],
       },
     },
   },
