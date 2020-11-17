9fdc5fce5abce71f907e3e14519fad3a97423683
Austin Walela <walela@mesozi.com>
2020-11-17 12:35:14 +0300

    added start script

--- public/app.js
+++ public/app.js
@@ -2,8 +2,14 @@ Vue.createApp({
   data() {
     return {
       forms: [
-        { title: "Form 001", questions: ["Question 01", "Question 02", "Question 03"] },
-        { title: "Form 002", questions: ["Question 01", "Question 02", "Question 03"] },
+        {
+          title: "Form 001",
+          questions: ["Question 01", "Question 02", "Question 03"],
+        },
+        {
+          title: "Form 002",
+          questions: ["Question 01", "Question 02", "Question 03"],
+        },
       ],
     };
   },
